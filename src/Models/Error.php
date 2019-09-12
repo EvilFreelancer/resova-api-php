587da93707b233f7188cb04ab792fdd17ab854dd
Paul Rock <paul@drteam.rocks>
2019-09-12 12:04:23 +0300

    error model added

---
+++ src/Models/Error.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Resova\Models;
+
+use Resova\Model;
+
+class Error extends Model
+{
+
+    /**
+     * List of allowed fields
+     *
+     * @return array
+     */
+    public function allowed(): array
+    {
+        return [
+            'type'              => 'string', // type of error returned. Can be: 'api_error', 'authentication_error', 'not_found_error', 'validation_error' or 'rate_limit_error'.
+            'message'           => 'string', // A message providing more details about the error.
+            'validation_failed' => 'string', // For validation errors this will provide details on what validation failed.
+        ];
+    }
+}
